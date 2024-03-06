f4093ac5e5778a008bc111466d53e34b10dd2e06
Mihalache Alexandru <mihalachealexandru25@yahoo.com>
2024-03-06 19:06:49 +0200

    Now the code is correct

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,16 +2,13 @@
 #include <stdio.h>
 
 int function(int x){
-    if(x<=0)
-        return x;
 	x=x-2;
-	x=function(x);
 	return x;
 }
 
 int main(){
 	int a=30, i=0;
-	
+
 	while (a>0){
 		a=function(a);
 		i++;
